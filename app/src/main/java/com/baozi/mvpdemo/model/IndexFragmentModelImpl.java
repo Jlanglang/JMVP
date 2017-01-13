1903253c79288ad91bbff06f81d43206a44fec37
Jlanglang <347891332@qq.com>
2017-01-13 11:10:11 +0800

    1.新增通用webview 2.新增处理弹出等待框的presenter 3.优化了basefragment和baseactivity 4.优化了basepresenter

--- app/src/main/java/com/baozi/mvpdemo/model/IndexFragmentModelImpl.java
+++ app/src/main/java/com/baozi/mvpdemo/model/IndexFragmentModelImpl.java
@@ -2,6 +2,7 @@ package com.baozi.mvpdemo.model;
 
 import com.baozi.mvpdemo.contract.IndexFragmentContract;
 import com.baozi.mvpdemo.location.APIServiceImpl;
+import com.baozi.mvpdemo.location.rxandroid.ModelFilterFactory;
 
 import java.util.HashMap;
 
@@ -15,6 +16,6 @@ public class IndexFragmentModelImpl implements IndexFragmentContract.Model {
 
     @Override
     public Observable login(HashMap<String, Object> parmas) {
-        return ModleFilterFactory.filter(APIServiceImpl.getInstance().login(parmas));
+        return ModelFilterFactory.compose(APIServiceImpl.getInstance().login(parmas));
     }
 }
