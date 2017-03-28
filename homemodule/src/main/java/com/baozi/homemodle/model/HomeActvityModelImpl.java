0b8085f46b6ebe7084188ed3085954bbe1eaa62b
Jlanglang <347891332@qq.com>
2017-03-28 10:27:04 +0800

    1.修改命名

---
+++ homemodule/src/main/java/com/baozi/homemodle/model/HomeActvityModelImpl.java
@@ -0,0 +1,10 @@
+package com.baozi.homemodle.model;
+import com.baozi.homemodle.contract.HomeActvityContract;
+
+/**
+* Created by Administrator on 2017/03/28
+*/
+
+public class HomeActvityModelImpl implements HomeActvityContract.Model{
+
+}
---
+++ homemodule/src/main/java/com/baozi/homemodle/presenter/HomeActvityPresenterImpl.java
@@ -0,0 +1,27 @@
+package com.baozi.homemodle.presenter;
+
+import com.baozi.homemodle.contract.HomeActvityContract;
+import com.baozi.jmvp.presenter.TempletPresenter;
+
+/**
+ * Created by Administrator on 2017/03/28
+ */
+
+public class HomeActvityPresenterImpl extends TempletPresenter<HomeActvityContract.View>
+        implements HomeActvityContract.Presenter {
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void loadData() {
+
+    }
+
+    @Override
+    public void cancleNetWork() {
+
+    }
+}
