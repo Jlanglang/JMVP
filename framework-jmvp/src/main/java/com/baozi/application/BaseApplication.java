b9d2b9aa002558342ef9a171a0de65920f812116
jlanglang <347891332@qq.com>
2017-10-16 17:11:22 +0800

    1.修改目录

---
+++ framework-jmvp/src/main/java/com/baozi/application/BaseApplication.java
@@ -0,0 +1,11 @@
+package com.baozi.application;
+
+import android.app.Application;
+import com.baozi.mvp.MVPManager;
+
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
