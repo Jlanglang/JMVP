fcb4943a32746f60c4813ec136b49eb2d871deab
Jlanglang <347891332@qq.com>
2018-11-20 14:51:22 +0800

    1.简单修改

--- lib-utils-common/src/main/java/com/linfeng/common/widget/SquareRelativeLayout.java
+++ lib-utils-common/src/main/java/com/linfeng/common/widget/SquareRelativeLayout.java
@@ -23,14 +23,8 @@ public class SquareRelativeLayout extends RelativeLayout {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        // For simple implementation, or internal size is always 0.
-        // We depend on the container to specify the layout size of
-        // our view. We can't really know what it is since we will be
-        // adding and removing different arbitrary views and do not
-        // want the layout to change as this happens.
         setMeasuredDimension(getDefaultSize(0, widthMeasureSpec), getDefaultSize(0, heightMeasureSpec));
 
-        // Children are just made to fill our space.
         int childWidthSize = getMeasuredWidth();
         widthMeasureSpec = MeasureSpec.makeMeasureSpec(childWidthSize, MeasureSpec.EXACTLY);
         heightMeasureSpec = widthMeasureSpec;
