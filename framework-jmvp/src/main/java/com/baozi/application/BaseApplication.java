1c9f3c30f1d441ed5724ee60e3b5e3fd56a365a2
Jlanglang <347891332@qq.com>
2019-01-11 15:03:53 +0800

    1.更新build

--- framework-jmvp/src/main/java/com/baozi/application/BaseApplication.java
+++ framework-jmvp/src/main/java/com/baozi/application/BaseApplication.java
@@ -1,11 +1,44 @@
 package com.baozi.application;
 
 import android.app.Application;
-import com.baozi.mvp.MVPManager;
+
+import com.baozi.location.JApiImpl;
+import com.linfeng.rx_retrofit_network.NetWorkManager;
+import com.linfeng.rx_retrofit_network.location.APICallBack;
+import com.linfeng.rx_retrofit_network.location.SimpleParams;
+import com.linfeng.rx_retrofit_network.location.model.BaseResponse;
+import com.linfeng.rx_retrofit_network.location.rxandroid.NetWorkTransformer;
+import com.linfeng.rx_retrofit_network.location.rxandroid.SimpleObserver;
 
 public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        //比如www.baidu.com
+        //3个参数,hostUrl,成功码,context
+        NetWorkManager.init("你的hostUrl", 0, this);
+        NetWorkManager.setApiCallBack(new APICallBack() {
+            @Override
+            public String callback(int code, BaseResponse baseResponse) {
+                //接口发生异常的回调.
+                return null;//返回什么弹什么.
+            }
+        });
+        //请求的例子
+        //比如www.baidu.com/api/a
+        JApiImpl.getJApi().BasePost("api/a",
+                SimpleParams.create()//请求参数集合
+                        .putP("key", "")
+                        .putP("key1", "")
+                        .putP("key2", "")
+                        .putP("key3", "")
+        )
+                .compose(new NetWorkTransformer<String>())//这里的泛型,写你需要解析的javabaen
+                .subscribe(new SimpleObserver<String>() {
+                    @Override
+                    public void call(String s) {
+                        //成功的回调
+                    }
+                });
     }
 }
