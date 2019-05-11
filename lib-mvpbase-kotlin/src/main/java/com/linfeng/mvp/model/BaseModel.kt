f839e25995a828a2d7384066a330a740ec4a6d7a
Jlanglang <347891332@qq.com>
2019-05-11 12:30:25 +0800

    1.拆分框架

--- lib-mvpbase-kotlin/src/main/java/com/linfeng/mvp/model/BaseModel.kt
+++
@@ -1,9 +0,0 @@
-package com.linfeng.mvp.model
-
-import com.linfeng.mvp.presenter.BasePresenter
-
-/**
- * Created by baozi on 2017/10/23.
- */
-
-class BaseModel<T : BasePresenter<*>>(protected var mPresenter: T)
--- lib-mvpbase-kotlin/src/main/java/com/linfeng/mvp/model/LoadDataModel.kt
+++
@@ -1,9 +0,0 @@
-package com.linfeng.mvp.model
-
-/**
- * Created by baozi on 2017/12/5.
- */
-
-interface LoadDataModel<T> {
-    fun loadData(pageNum: Int, pageSize: Int, isRefresh: Boolean): T
-}
