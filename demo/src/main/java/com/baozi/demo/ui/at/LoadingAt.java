a32b502b8fc1c78c4a0fa762baf8396fa1c9c326
Jlanglang <347891332@qq.com>
2019-07-31 17:46:25 +0800

    1.添加loading

--- demo/src/main/java/com/baozi/demo/ui/at/LoadingAt.java
+++ demo/src/main/java/com/baozi/demo/ui/at/LoadingAt.java
@@ -2,12 +2,16 @@ package com.baozi.demo.ui.at;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.baozi.demo.R;
 import com.baozi.demo.persenter.MainPresenter;
 import com.baozi.mvp.annotation.JView;
 import com.baozi.mvp.tempalet.TemplateActivity;
 import com.baozi.mvp.tempalet.options.ContentOptions;
+import com.bumptech.glide.Glide;
 
 @JView(layout = R.layout.at_load, p = MainPresenter.class, openLoading = true)
 public class LoadingAt extends TemplateActivity<MainPresenter> {
@@ -45,14 +49,14 @@ public class LoadingAt extends TemplateActivity<MainPresenter> {
         }.start();
     }
 
-//    @Override
-//    protected View wrapperContentView(View view) {
-//        ImageView imageView = new ImageView(this);
-//        imageView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-//        ((ViewGroup) view).addView(imageView, 0);
-//        Glide.with(this).load("file:///android_asset/a.gif").into(imageView);
-//        return super.wrapperContentView(view);
-//    }
+    @Override
+    protected View wrapperContentView(View view) {
+        ImageView imageView = new ImageView(this);
+        imageView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        ((ViewGroup) view).addView(imageView, 0);
+        Glide.with(this).load("file:///android_asset/a.gif").into(imageView);
+        return super.wrapperContentView(view);
+    }
 
     @Override
     public ContentOptions getContentOptions() {
